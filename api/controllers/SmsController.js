7a4f1a66ec8c7d58bbb72914f717291f3c97fb23
dylantg <dgraham@placeiq.com>
2014-12-06 17:56:37 -0500

    Twilio setup modular

--- api/controllers/SmsController.js
+++ api/controllers/SmsController.js
@@ -6,7 +6,7 @@
  */
 
 var twilio = require("/usr/local/lib/node_modules/twilio/lib");
-var client = new twilio.RestClient('AC7e83dcc644f2db1aba8831700b2ab78d', '1392668645e3c5ce5895aa67e052032c');
+var client = new twilio.RestClient(sails.config.twilioConfig.key, sails.config.twilioConfig.key.secret);
 
 
 module.exports = {
@@ -14,17 +14,23 @@ module.exports = {
 		var data = req.body;
 		console.log(data["Body"]);
 		console.log(data["From"]);
-		// console.log(JSON.stringify(req.body));
-		res.end();
+		// res.end();
+		// TEST: Currently don't return a picture
+		var image_found = false;
+		if (image_found){
+			var body_text = "Here is a picture of " + data["Body"];
+			var image_url = "";
+		} else {
+			var body_text = "Can't find a picture of " + data["Body"] + " yet. Check back later"
+			var image_url = sails.config.twilioConfig.default_pic
+		}
 
 // SMS Message sender from earlier (TEST)
 	client.messages.create({
-    // body: "Searching for a picture of ".concat(data["Body"]),
-    body: "Can't find a picture of " + data["Body"] + " yet. Check back later",
+    body: body_text,
     to: data["From"],
     from: '+15854818566',
-    // mediaUrl: "http://www.example.com/hearts.png"
-   mediaUrl: "http://thumb7.shutterstock.com/display_pic_with_logo/1284574/194061785/stock-photo-astronaut-on-the-moon-elements-of-this-image-furnished-by-nasa-194061785.jpg"
+    mediaUrl: image_url
 }, function(err, message) {
     process.stdout.write(message.sid);
     console.log("\nMessage sent")
---
+++ config/twilioConfig.js
@@ -0,0 +1,6 @@
+module.exports.twilioConfig = {
+	key : 'AC7e83dcc644f2db1aba8831700b2ab78d',
+	secret : '1392668645e3c5ce5895aa67e052032c',
+	default_pic: "http://thumb7.shutterstock.com/display_pic_with_logo/1284574/194061785/stock-photo-astronaut-on-the-moon-elements-of-this-image-furnished-by-nasa-194061785.jpg"
+}
+
