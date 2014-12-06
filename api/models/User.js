812d9ddf6492931f839b81d819af4130dff63c02
Arunram Kalaiselvan <arunram@placeiq.com>
2014-12-06 15:01:24 -0500

    Installed sails-generate-auth that does the login stuff for us

--- api/models/User.js
+++ api/models/User.js
@@ -1,38 +1,12 @@
-/**
-* User.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
-
-module.exports = {
+var User = {
+  // Enforce model schema in the case of schemaless databases
+  schema: true,
 
   attributes: {
-    email: {
-      type: 'string',
-      unique: true,
-      required: true
-    },
-    password: {
-      type: 'string',
-      required: true,
-      minLength: 6
-    }
-  },
-
-  beforeCreate: function (attrs, next) {
-    var bcrypt = require('bcrypt');
-
-    bcrypt.genSalt(10, function(err, salt) {
-      if (err) return next(err);
-
-      bcrypt.hash(attrs.password, salt, function(err, hash) {
-        if (err) return next(err);
-
-        attrs.password = hash;
-        next();
-      });
-    });
+    username  : { type: 'string', unique: true },
+    email     : { type: 'email',  unique: true },
+    passports : { collection: 'Passport', via: 'user' }
   }
 };
 
+module.exports = User;
