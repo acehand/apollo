35ba9b3e8521324bedc95b14e3c72d4e43bd4e4d
Anand <anandc.1988@gmai.com>
2014-12-06 14:18:55 -0500

    Adding a Search Controller, adding a test route

---
+++ api/controllers/SearchController.js
@@ -0,0 +1,41 @@
+/**
+ * SearchController
+ *
+ * @description :: Server-side logic for managing searches
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `SearchController.getTags()`
+   */
+  getTags: function (req, res) {
+    return res.json({
+      todo: 'getTags() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `SearchController.getImages()`
+   */
+  getImages: function (req, res) {
+    return res.json({
+      todo: 'getImages() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `SearchController.getSearchAttributes()`
+   */
+  getSearchAttributes: function (req, res) {
+    return res.json({
+      todo: 'getSearchAttributes() is not implemented yet!'
+    });
+  }
+};
+
