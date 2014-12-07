bf68a5d2ddf5a81520bb662b557fb97e0481a597
Anand <anandc.1988@gmai.com>
2014-12-07 02:14:46 -0500

    Committing before pull

--- api/controllers/SearchController.js
+++ api/controllers/SearchController.js
@@ -7,6 +7,7 @@ module.exports = {
     var url = sails.config.imaggaConfig.tag_endpoint+'?url=http%3A%2F%2Fwww.visitgarda.com%2FUpload%2Fcms%2F400_x%2FMountain%2520bike%2520sul%2520Lago%2520di%2520Garda(1).jpg';
     var imagga_request = rest.get(url, sails.config.imaggaConfig.headers);
     console.log(imagga_request.headers);
+    Log.info("requesting Image");
     imagga_request.on('success',function(result){
       console.log('Image tags Fetched');
     });
@@ -21,15 +22,16 @@ module.exports = {
 
   getImages: function (req, res) {
     //Add page numbers to DB later
-    var search_term = '?query="landscape travel"';
+    var search_term = '?query="landscape Travel"';
     var term = "landscape travel";
     var url = baseParams.shutterStock.base_url + search_term;
     var images = rest.get(url, baseParams.shutterStock.headers);
     images.on('success',function(result){
-      Images.addImage(result,term, 'SHUTTERSTOCK');
+      // Images.addImage(result,term, 'SHUTTERSTOCK');
     });
     images.on('fail',function(result){
      console.log('No results');
+     console.log(result);
     });
     images.on('complete',function(result){
         return res.json({
