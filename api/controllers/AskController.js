9a3fb883f14a4e11c193cad69971fa844fd78095
Arunram Kalaiselvan <arunram@placeiq.com>
2014-12-07 11:50:47 -0500

    getting started with requests

---
+++ api/controllers/AskController.js
@@ -0,0 +1,11 @@
+/**
+ * AskController
+ *
+ * @description :: Server-side logic for managing asks
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Ask.js
@@ -0,0 +1,25 @@
+/**
+* Ask.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+var Ask = {
+  schema: true,
+
+  attributes: {
+    lat: 'float',
+    lon: 'float',
+    search: 'string',
+    user_id: 'string',
+    fulfilled: 'boolean'
+  },
+
+  searchByLocation: function(lat,lon, radius) {
+    radius = radius || 100;
+
+  }
+};
+
+module.exports = Ask;
